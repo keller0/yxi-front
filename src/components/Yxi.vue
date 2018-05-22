4ffa360a24102e247836ba1a04aa6ecf5083b0a3
caijiaqi <caijiaqi2017@gmail.com>
2018-05-22 23:58:48 +0800

    add event

--- src/components/Yxi.vue
+++ src/components/Yxi.vue
@@ -11,8 +11,9 @@
   </el-aside>
   <el-main>
     <el-card class="box-card">
-      <codemirror v-model="code" :options="editorOption" @change="chnageCode" ></codemirror>
+      <codemirror v-model="code" :options="editorOption" ref="ondeEditor" @change="chnageCode" ></codemirror>
     </el-card>
+     <el-button type="primary" @click="runcode(code)">运行</el-button>
   </el-main>
 </el-container>
 
@@ -25,6 +26,9 @@ require('codemirror/mode/javascript/javascript.js')
 require('codemirror/addon/hint/show-hint.js')
 require('codemirror/addon/hint/show-hint.css')
 require('codemirror/addon/hint/javascript-hint.js')
+
+require('codemirror/mode/clike/clike.js')
+
 // themes need to be inlcuded
 // require('codemirror/theme/blackboard.css')
 export default {
@@ -36,7 +40,7 @@ export default {
     return {
       code: 'const str = "hello world"',
       editorOption: {
-        mode: 'javascript',
+        mode: 'clike',
         lineNumbers: true,
         lineWrapping: true,
         extraKeys: {'Ctrl-Space': 'autocomplete'}
@@ -48,6 +52,9 @@ export default {
   methods: {
     chnageCode: function (e) {
       console.log(e)
+    },
+    runcode: function (c) {
+      console.log(c)
     }
   }
 }
