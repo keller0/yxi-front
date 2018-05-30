f81091f4c3866c7cd400e706d1ebd2df6fafcb7e
zhanglianhao <15524629698@163.com>
2018-05-30 18:52:49 +0800

    modify router
    modify router, eslint rules and directory structure, add axios globall configuration

--- src/main.js
+++ src/main.js
@@ -2,33 +2,20 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import router from './router'
 
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 
-import VueRouter from 'vue-router'
-
-import Home from './components/Home'
-
-Vue.use(VueRouter)
 Vue.use(Vuetify)
 
-const routes = [
-  {path: '/', component: Home}
-]
-
-const router = new VueRouter({
-  routes,
-  mode: 'history'
-})
-
 new Vue({
-  el: '#app',
-  router,
-  components: {
-    App
-  },
-  template: '<App/>',
-  render: h => h(App)
+    el: '#app',
+    router,
+    components: {
+        App
+    },
+    template: '<App/>',
+    render: h => h(App)
 
 }).$mount('#app')
