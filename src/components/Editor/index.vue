fe5dba165ec42e58be1980784b2ff8a65d660c23
zhanglianhao <15524629698@163.com>
2018-05-31 17:24:08 +0800

    modify api and related file

--- src/components/Editor/index.vue
+++ src/components/Editor/index.vue
@@ -1,32 +1,32 @@
 <template>
-	<v-card width="100%">
-		<v-card-title primary-title>
-			<div>
-				<h3 class="headline">The title</h3>
-				<div>description</div>
-			</div>
-		</v-card-title>
-		<v-spacer></v-spacer>
-		<v-card-text>
-			<codemirror v-model="code" :options="cmOption" ></codemirror>
-			<v-progress-linear :indeterminate="runBar" :value="pecBar"></v-progress-linear>
-			<v-card-actions>
-				<v-btn color="blue" @click="runcode">Run</v-btn>
-				<v-spacer></v-spacer>
-				<v-btn icon @click.native="rshow = !rshow">
-					<v-icon>{{ rshow ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
-				</v-btn>
-			</v-card-actions>
-				<v-slide-y-transition>
-					<v-card-text v-show="rshow">
-		  <v-alert v-model="errShow" type="error" dismissible>
-					  {{error}}
-					</v-alert>
-					<textarea v-model="result" placeholder="nothing..."></textarea>
-				</v-card-text>
-			</v-slide-y-transition>
-		</v-card-text>
-	</v-card>
+    <v-card width="100%">
+        <v-card-title primary-title>
+            <div>
+                <h3 class="headline">The title</h3>
+                <div>description</div>
+            </div>
+        </v-card-title>
+        <v-spacer></v-spacer>
+        <v-card-text>
+            <codemirror v-model="code" :options="cmOption" ></codemirror>
+            <v-progress-linear :indeterminate="runBar" :value="pecBar"></v-progress-linear>
+            <v-card-actions>
+                <v-btn color="blue" @click="runcode">Run</v-btn>
+                <v-spacer></v-spacer>
+                <v-btn icon @click.native="rshow = !rshow">
+                    <v-icon>{{ rshow ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
+                </v-btn>
+            </v-card-actions>
+                <v-slide-y-transition>
+                    <v-card-text v-show="rshow">
+                    <v-alert v-model="errShow" type="error" dismissible>
+                        {{error}}
+                    </v-alert>
+                    <textarea v-model="result" placeholder="nothing..."></textarea>
+                </v-card-text>
+            </v-slide-y-transition>
+        </v-card-text>
+    </v-card>
 </template>
 
 <script>
@@ -35,74 +35,75 @@ import 'codemirror/mode/clike/clike.js'
 import 'codemirror/lib/codemirror.css'
 import 'codemirror/theme/blackboard.css'
 
-import { resultC } from '@/api/langC'
+import { codeRunResult } from '@/api/codeResult'
 
 const code = '#include<stdio.h>\n\nint main() {\n    printf("hello");\n}'
 
 export default {
-	components: {
-		codemirror
-	},
-	data: function() {
-		return {
-			name: 'Edit',
-			errShow: false,
-			rshow: false,
-			error: '',
-			result: '',
-			code,
-			runBar: false,
-			pecBar: 0,
-			cmOption: {
-				tabSize: 4,
-				lineNumbers: true,
-				line: true,
-				mode: 'text/x-csrc',
-				theme: this.editerTheme
-			}
-		}
-	},
-	methods: {
-		async runcode() {
-			try {
-				this.statusStartRun()
-				const data = {
-					files: [
-						{
-							content: this.code,
-							name: 'main.c'
-						}
-					],
-					stdin: '',
-					argument: ''
-				}
-				const res = await resultC(data)
-				this.result += res.stdout
-				this.result += res.stderr
-				this.statusStopRun()
-			} catch (error) {
-				this.errShow = true
-				this.error = error.message
-				console.error(error)
-				this.statusStopRun()
-			}
-		},
-		statusStartRun: function() {
-			this.result = ""
-			this.runBar = true
-			this.rshow = false
-		},
-		statusStopRun: function() {
-			this.runBar = false,
-			this.pecBar = 100
-			this.rshow = true
-	}
-},
-	props: [
-		'editerTheme',
-		'titile',
-		'desc'
-	]
+    name: 'Editor',
+    components: {
+        codemirror
+    },
+    props: [
+        'editerTheme',
+        'titile',
+        'desc'
+    ],
+    data: function() {
+        return {
+            name: 'Edit',
+            errShow: false,
+            rshow: false,
+            error: '',
+            result: '',
+            code,
+            runBar: false,
+            pecBar: 0,
+            cmOption: {
+                tabSize: 4,
+                lineNumbers: true,
+                line: true,
+                mode: 'text/x-csrc',
+                theme: this.editerTheme
+            }
+        }
+    },
+    methods: {
+        async runcode() {
+            try {
+                this.statusStartRun()
+                const data = {
+                    files: [
+                        {
+                            content: this.code,
+                            name: 'main.c'
+                        }
+                    ],
+                    stdin: '',
+                    argument: ''
+                }
+                const res = await codeRunResult('/c', data)
+                this.result += res.stdout
+                this.result += res.stderr
+            } catch (error) {
+                this.errShow = true
+                this.error = error.message
+                console.error(error)
+            } finally {
+                this.statusStopRun()
+            }
+        },
+        statusStartRun: function() {
+            this.result = ''
+            this.runBar = true
+            this.rshow = false
+        },
+        statusStopRun: function() {
+            this.runBar = false
+            this.pecBar = 100
+            this.rshow = true
+        }
+    }
 }
 
 </script>
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,85 +1,81 @@
 <template>
-  <v-app
-    id="home"
+    <v-app id="home">
+        <v-navigation-drawer
+            :clipped="$vuetify.breakpoint.lgAndUp"
+            v-model="drawer"
+            fixed
+            app>
+            <v-list dense>
+                <v-list-tile v-for="item in items" :key="item.text" @click="goPage(item.text)">
+                    <v-list-tile-action>
+                        <v-icon>{{ item.icon }}</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                        <v-list-tile-title>{{ item.text }}</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
 
-  >
-    <v-navigation-drawer
-      :clipped="$vuetify.breakpoint.lgAndUp"
-      v-model="drawer"
-      fixed
-      app
-    >
-      <v-list dense>
-        <v-list-tile v-for="item in items" :key="item.text" @click="goPage(item.text)">
-          <v-list-tile-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>
-              {{ item.text }}
-            </v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
+                <v-list-tile @click="goPage('Settings')">
+                    <v-list-tile-action>
+                        <v-icon color="grey darken-1">settings</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-title class="grey--text text--darken-1">Settings</v-list-tile-title>
+                </v-list-tile>
+            </v-list>
+        </v-navigation-drawer>
+        <v-toolbar
+            color=""
+            dense
+            fixed
+            clipped-left
+            app>
+            <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+            <v-icon class="mx-3"></v-icon>
+            <v-toolbar-title class="mr-5 align-center">
+                <span class="title">YXI</span>
+            </v-toolbar-title>
+        </v-toolbar>
 
-        <v-list-tile @click="goPage('Settings')">
-          <v-list-tile-action>
-            <v-icon color="grey darken-1">settings</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title class="grey--text text--darken-1">Settings</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar
-      color=""
-      dense
-      fixed
-      clipped-left
-      app
-    >
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-icon class="mx-3"></v-icon>
-      <v-toolbar-title class="mr-5 align-center">
-        <span class="title">YXI</span>
-      </v-toolbar-title>
-
-    </v-toolbar>
-
-    <v-content >
-      <v-container >
-        <v-layout row justify-center>
-          <!-- the editor -->
-          <v-flex md10 offset-md1 sm12>
-            <Edit :editerTheme="edittheme" > </Edit>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-content>
-  </v-app>
+        <v-content >
+            <v-container >
+                <v-layout row justify-center>
+                    <!-- the editor -->
+                    <v-flex md10 offset-md1 sm12>
+                        <Edit :editerTheme="edittheme" > </Edit>
+                    </v-flex>
+                </v-layout>
+            </v-container>
+        </v-content>
+    </v-app>
 </template>
 
 <script>
 import Edit from '@/components/Editor'
 export default {
-  components: {
-    Edit
-  },
-  data: () => ({
-    name: 'home',
-    drawer: null,
-    edittheme: "blackboard",
-    items: [
-      { icon: 'trending_up', text: 'Popular' },
-      { icon: 'public', text: 'Public' },
-      { icon: 'folder', text: 'Mine' },
-      { icon: 'help', text: 'Help' }
-    ]
-  }),
-  props: {
-    source: String,
-  },methods: {
-    goPage: function(a) {
-      console.log(a)
+    name: 'Home',
+    components: {
+        Edit
+    },
+    data() {
+        return {
+            name: 'home',
+            drawer: null,
+            edittheme: 'blackboard',
+            items: [
+                { icon: 'trending_up', text: 'Popular' },
+                { icon: 'public', text: 'Public' },
+                { icon: 'folder', text: 'Mine' },
+                { icon: 'help', text: 'Help' }
+            ]
+        }
+    },
+    props: {
+        source: String
+    },
+    methods: {
+        goPage: function(a) {
+            console.log(a)
+        }
     }
-  }
 }
 </script>
