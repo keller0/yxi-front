9735ff46425fc02affdd640bdc9cd65740c8ad8d
zhanglianhao <15524629698@163.com>
2018-06-05 17:58:17 +0800

    the editor adds theme toggle

--- src/components/Editor/index.vue
+++ src/components/Editor/index.vue
@@ -1,8 +1,18 @@
 <template>
     <v-card width="100%">
-        <v-card-title primary-title>
+        <v-card-title primary-title style="justify-content: space-between;">
+            <h3 class="headline">"Hello, world"</h3>
             <div>
-                <h3 class="headline">"Hello, world"</h3>
+                <v-select
+                    v-model="select"
+                    label="Select"
+                    item-text="type"
+                    item-value="value"
+                    single-line
+                    return-object
+                    :items="themes"
+                    @change="test">
+                </v-select>
             </div>
         </v-card-title>
         <v-spacer></v-spacer>
@@ -21,7 +31,6 @@ import 'codemirror/lib/codemirror.css'
 import 'codemirror/theme/blackboard.css'
 import runCode from '@/components/Editor/runCode'
 import newButton from '@/components/newButton'
-const code = '#include<stdio.h>\n\nint main()\n{\n  printf("Hello, World!\\n");\n}'
 export default {
     name: 'Editor',
     components: {
@@ -29,21 +38,34 @@ export default {
         runCode,
         newButton
     },
-    props: [
-    ],
-    data: function() {
+    data() {
         return {
-            code,
+            code: `#include<stdio.h>\n\nint main()\n{\n  printf("Hello, World!\\n");\n}`,
             cmOption: {
                 tabSize: 4,
                 lineNumbers: true,
                 line: true,
                 mode: 'text/x-csrc',
                 theme: 'blackboard'
-            }
+            },
+            select: { type: 'blackboard', value: 'blackboard.css' },
+            themes: [
+                { type: 'default', value: '' },
+                { type: 'blackboard', value: 'blackboard.css' },
+                { type: '3024-day', value: '3024-day.css' },
+                { type: '3024-night', value: '3024-night.css' },
+                { type: 'base16-dark', value: 'base16-dark.css' },
+                { type: 'base16-light', value: 'base16-light.css' },
+                { type: 'ambiance-mobile', value: 'ambiance-mobile.css' },
+                { type: 'ambiance', value: 'ambiance.css' }
+            ]
         }
     },
     methods: {
+        test(theme) {
+            import(`codemirror/theme/${theme.value}`)
+            this.cmOption.theme = theme.type
+        }
     }
 }
 </script>
