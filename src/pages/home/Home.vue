7a669a4c225b068b7e2c0004623f689e9bcf1589
caijiaqi <caijiaqi2017@gmail.com>
2018-06-13 08:34:40 +0800

    change header, use a simple layout

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,57 +1,20 @@
 <template>
-    <v-app id="home">
-        <v-navigation-drawer
-            :clipped="$vuetify.breakpoint.lgAndUp"
-            v-model="drawer"
-            fixed
-            app>
-            <v-list dense>
-                <v-list-tile
-                    v-show="!$vuetify.breakpoint.lgAndUp"
-                    @click="goHome"
-                    @click.stop="drawer = !drawer">
-                    <v-list-tile-action>
-                        <v-icon color="grey darken-1">home</v-icon>
-                    </v-list-tile-action>
-                    <v-list-tile-title class="grey--text text--darken-1">Home</v-list-tile-title>
-                </v-list-tile>
-
-                 <v-list-tile v-for="menu in menus" :key="menu.path" @click="goPage(menu.path)">
-                    <v-list-tile-action>
-                        <v-icon>{{ menu.icon }}</v-icon>
-                    </v-list-tile-action>
-                    <v-list-tile-content>
-                        <v-list-tile-title>{{ menu.title }}</v-list-tile-title>
-                    </v-list-tile-content>
-                </v-list-tile>
-
-                <v-list-tile @click="goPage('/help')">
-                    <v-list-tile-action>
-                        <v-icon color="grey darken-1">help</v-icon>
-                    </v-list-tile-action>
-                    <v-list-tile-title class="grey--text text--darken-1">Help</v-list-tile-title>
-                </v-list-tile>
-                <v-list-tile>
-                    <v-list-tile-action>
-                        <v-icon color="grey darken-1">settings</v-icon>
-                    </v-list-tile-action>
-                    <v-list-tile-title class="grey--text text--darken-1">Settings</v-list-tile-title>
-                </v-list-tile>
-            </v-list>
-        </v-navigation-drawer>
-
+    <v-app>
         <v-toolbar
-            color=""
+            color="blue"
             dense
             fixed
-            clipped-left
             app>
-            <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-            <v-icon class="mx-3"></v-icon>
-            <v-toolbar-title class="mr-5 align-center">
+            <v-toolbar-title class="">
                 <!-- an icon ? -->
                 <span class="title" @click="goHome" style="cursor: pointer;">YXI</span>
             </v-toolbar-title>
+            <v-spacer class=""></v-spacer>
+            <v-toolbar-items >
+                <v-btn flat @click="goPage('/popular')"><v-icon left dark>trending_up</v-icon>Top</v-btn>
+                <v-btn flat @click="goPage('/public')"><v-icon left dark>public</v-icon>Pub</v-btn>
+                <v-btn flat @click="goPage('/mine')">My Code</v-btn>
+            </v-toolbar-items>
         </v-toolbar>
 
         <v-content >
@@ -68,17 +31,9 @@
 export default {
     name: 'Home',
     props: {
-        source: String
     },
     data() {
         return {
-            name: 'home',
-            drawer: false,
-            menus: [
-                { icon: 'trending_up', title: 'Popular', path: '/popular' },
-                { icon: 'public', title: 'Public', path: '/public' },
-                { icon: 'folder', title: 'Mine', path: '/mine' }
-            ]
         }
     },
     methods: {
