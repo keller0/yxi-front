84cc9c1d930d83323bd0820dec550a6b8b9f1c08
caijiaqi <caijiaqi2017@gmail.com>
2018-06-22 23:03:00 +0800

    use footer at all pages

--- src/pages/Sample.vue
+++ src/pages/Sample.vue
@@ -6,15 +6,19 @@
                 <router-view></router-view>
             </v-container>
         </v-content>
+        <Footer></Footer>
     </v-app>
 </template>
 
 <script>
 import Header from '@/components/Header'
 
+import Footer from '@/components/Footer'
+
 export default {
     components: {
-        Header
+        Header,
+        Footer
     },
     props: {
     },
