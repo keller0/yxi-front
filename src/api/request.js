5446e736bd4d78b55553ce8fbd31554d83143029
caijiaqi <caijiaqi2017@gmail.com>
2018-07-07 02:44:25 +0800

    update run code api

--- src/api/request.js
+++ src/api/request.js
@@ -1,54 +1,7 @@
 import axios from 'axios'
-const RunBaseURL = 'https://r.yxi.io/v1'
+
 const APIBaseURL = 'https://api.yxi.io/v1'
-
-const runcodeService = axios.create({
-    baseURL: RunBaseURL,
-    timeout: 10 * 1000,
-    withCredentials: false
-})
-
-runcodeService.interceptors.request.use(config => {
-    config.headers['Api-Key'] = 'sharedkey'
-    return config
-}, error => {
-    return Promise.reject(error)
-})
-
-runcodeService.interceptors.response.use(response => {
-    if (response.status === 200) {
-        return Promise.resolve(response.data)
-    }
-    return Promise.reject(response)
-}, error => {
-    if (error && error.response) {
-        switch (error.response.status) {
-            case 400:
-                error.message = '请求错误'
-                break
-
-            case 403:
-                error.message = '权限不足'
-                break
-
-            case 500:
-                error.message = '服务器错误'
-                break
-
-            default:
-        }
-    }
-
-    if (error && error.code === 'ECONNABORTED') {
-        error.message = '请求超时'
-    }
-
-    if (error && error.message === 'Network Error') {
-        error.message = '网络错误'
-    }
-
-    return Promise.reject(error)
-})
+// const APIBaseURL = 'http://localhost:8090/v1'
 
 const apiService = axios.create({
     baseURL: APIBaseURL,
@@ -65,4 +18,4 @@ apiService.interceptors.response.use(response => {
     return Promise.reject(error)
 })
 
-export { runcodeService, apiService }
+export { apiService }
